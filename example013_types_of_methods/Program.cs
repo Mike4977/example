130b2641d089f5c26a4106ca773b4865a19d1c20
mike77 <weilliss@mail.ru>
2023-02-19 16:37:51 +0600

    add types of method

---
+++ example013_types_of_methods/Program.cs
@@ -0,0 +1,59 @@
+﻿// //Method 1
+
+// void Method1()
+// {
+//     Console.WriteLine("Авторство этого мира принадлежит им");
+// }
+// Method1();
+
+
+// //Method 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("какой-то текст");
+
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//          i++;
+//     }
+// }
+// //Method21(msg: "какой-то текст", count: 4);
+// // Можно поменять местами явные вводные данные
+// Method21(count: 4, msg: "какой-то текст");
+
+
+
+// // Method 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+// Method 4
+string Method4(int count, string text)
+{
+    int  i = 0;
+    string result = String.Empty; // пустая строка
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4( 5, "asd ");
+Console.WriteLine(res);
+
